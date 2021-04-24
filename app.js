1620e9cad936661dc0de67dbc0216aa0f93ab170
MelinaGimenez <gimenez.melina@hotmail.com>
2021-04-24 20:29:58 -0300

    Agrego productDetail HTML/CSS

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const publicPath = path.resolve('./public')
 app.use(express.static(publicPath));
 
 const indexPath = path.resolve('./views/index.html');
-app.get('/', (req,res) => res.sendFile(indexPath));
+app.get('/', (req, res) => res.sendFile(indexPath));
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+});
 
 app.listen(3001, () => console.log('Servidor corriendo en el puerto 3001'));
