a7e061e1c50c148fd2f59d92c5ff544dbc4da217
santiagoGuastavino <san.guasta@gmail.com>
2021-05-02 19:44:57 -0300

    estructura html y links implementados

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// public
 const publicPath = path.resolve('./public')
 app.use(express.static(publicPath));
 
+// home
 const indexPath = path.resolve('./views/index.html');
 app.get('/', (req, res) => res.sendFile(indexPath));
 
-const registerPath = path.resolve('./views/register.html')
-app.get('/registro', (req, res) => res.sendFile(registerPath));
-
+// detalle de producto
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productDetail.html'));
 });
 
-app.get('/register', (req, res) => {
+// registro
+app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'));
 });
+
+// login
+const loginPath = path.resolve('./views/login.html');
+app.get('/login', (req,res) => res.sendFile(loginPath));
+
+// carrito de compras
+const cartPath = path.resolve('./views/shopping-cart.html');
+app.get('/carrito', (req,res) => res.sendFile(cartPath));
 
 app.listen(3001, () => console.log('Servidor corriendo en el puerto 3001'));
