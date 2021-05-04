e87cce5d63bb4a83f4ce7d350301137cc5142491
MelinaGimenez <gimenez.melina@hotmail.com>
2021-05-04 01:14:59 -0300

    varios detalle de producto

--- app.js
+++ app.js
@@ -10,9 +10,24 @@ app.use(express.static(publicPath));
 const indexPath = path.resolve('./views/index.html');
 app.get('/', (req, res) => res.sendFile(indexPath));
 
-// detalle de producto
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+// detalle de producto Hades
+app.get('/productDetailHades', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetailHades.html'));
+});
+
+// detalle de producto Cyberpunk
+app.get('/productDetailCyberpunk', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetailCyberpunk.html'));
+});
+
+// detalle de producto Stardew Valley
+app.get('/productDetailStardew', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetailStardew.html'));
+});
+
+// detalle de producto Sekiro
+app.get('/productDetailSekiro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetailSekiro.html'));
 });
 
 // registro
