d266035869b7bd6f39e5458bad42abf3659321ad
MelinaGimenez <gimenez.melina@hotmail.com>
2021-05-15 10:32:34 -0300

    css creacion de producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,4 @@
 let productController = {
-    renderProduct: (req, res) => {
-        let title = 'Hades';
-        res.render('./products/Hades', {'title': title})
-    },
     renderCart: (req, res) => {
         let title = 'Carrito de compras';
         res.render('./products/shopping-cart', {'title': title})
@@ -10,6 +6,10 @@ let productController = {
     renderNewProduct: (req, res) => {
         let title = 'Nuevo producto';
         res.render('./products/new-product', {'title': title})
+    },
+    renderProduct: (req, res) => {
+        let title = 'Hades';
+        res.render('./products/Hades', {'title': title})
     }
 }
 
