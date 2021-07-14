56fec8eac5e3dd2ffcf1aa13f7f12b526c0481df
santiagoGuastavino <san.guasta@gmail.com>
2021-07-13 22:24:23 -0300

    populating tables

---
+++ src/database/scripts/platforms.sql
@@ -0,0 +1,4 @@
+INSERT INTO platforms (id, title) VALUES (1, 'WINDOWS');
+INSERT INTO platforms (id, title) VALUES (2, 'MAC OS');
+INSERT INTO platforms (id, title) VALUES (3, 'LINUX');
+INSERT INTO platforms (id, title) VALUES (4, 'VR HEADSET');
---
+++ src/database/scripts/status.sql
@@ -0,0 +1,3 @@
+INSERT INTO status (id, name) VALUES (1, 'NONE');
+INSERT INTO status (id, name) VALUES (2, 'RELEVANT');
+INSERT INTO status (id, name) VALUES (3, 'IN OFFER');
