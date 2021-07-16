e5a65bf5ac7f23e9a6f93381c6560822f82f47e3
matiasncocco <matiasnicolascocco95@gmail.com>
2021-07-16 06:24:08 -0300

    Game model still not working

--- src/database/modelos a implementar/game.js
+++ src/database/modelos a implementar/game.js
@@ -1,59 +1,61 @@
 module.exports = (sequelize ,dataTypes) => {
 
-   let alias = "games";
+    let alias = "Game";
 
-   let columns = {
+    let columns = {
    
-    id: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false
-    },
-    title: {
+     id: {
+         autoIncrement: true,
+         primaryKey: true,
+         type: DataTypes.INTEGER.UNSIGNED,
+         allowNull: false
+     },
+     title: {
         type: DataTypes.STRING(255),
         allowNull: false
     },
-    img: {
-        type: dataTypes.STRING
-    },
-    price: {
-        type: dataTypes.INTEGER,
-        allowNull: false
-    },
-    disconunt: {
-        type: dataTypes.INTEGER,
-        allowNull: false
-    },
-     description: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        field: 'created_at'
-    },
-    updatedAt: {
-        type: DataTypes.DATE,
-        field: 'updated_at'
-    },
-    deletedAt: {
-        type: DataTypes.DATE, 
-        field: 'deleted_at'
-    }
-   };
+     img: {
+         type: dataTypes.STRING
+     },
+     price: {
+         type: dataTypes.INTEGER,
+         allowNull: false
+     },
+     disconunt: {
+         type: dataTypes.INTEGER,
+         allowNull: false
+     },
+      description: {
+         type: DataTypes.STRING(500),
+         allowNull: false
+     },
+     createdAt: {
+         type: DataTypes.DATE,
+         field: 'created_at'
+     },
+     updatedAt: {
+         type: DataTypes.DATE,
+         field: 'updated_at'
+     },
+     deletedAt: {
+         type: DataTypes.DATE, 
+         field: 'deleted_at'
+     }
+    };
 
-   // CONFIGURACION -----------------------------------------------------------------------------------------------------------------
+    
    
-   let config = {
-       timestamps: true
-   }
+    let config = {
+        tablename: 'games',
+        timestamps: false,
+        paranoid: false,
+        charset: 'utf8',
+        dialectOptions: {
+            collate: 'utf8mb4_unicode:ci'
+        }
+    };
 
-   const user = sequelize.define(alias, columns, config)
+    const Game = sequelize.define(alias, columns, config)
 
-   return user;
-}
-
-
-
-  
+    return Game;
+ }
