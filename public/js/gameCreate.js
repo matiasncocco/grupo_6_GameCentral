9b5802f4d36c0399b0a645bcd79f2297a37fe87d
santiagoGuastavino <san.guasta@gmail.com>
2021-08-11 11:17:18 -0300

    pruebas con interacciones

--- public/js/gameCreate.js
+++ public/js/gameCreate.js
@@ -126,3 +126,13 @@ form.addEventListener('submit', (e) => {
         form.submit();
     };
 });
+
+// console.log(form.title);
+let title = form.title;
+console.log(title);
+
+if (title.value.length < 5) {
+    title.addEventListener('blur', () => {
+        alert('hola');
+    })
+}
