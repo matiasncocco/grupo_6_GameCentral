16c8001e12aaa6f82539ee9595beaed36c39172d
santiagoGuastavino <san.guasta@gmail.com>
2021-08-17 19:13:48 -0300

    front validations incoming

--- public/js/validations/register.js
+++ public/js/validations/register.js
@@ -25,33 +25,112 @@ fetch('http://localhost:3001/api/users/email', settings)
 // voy a validar los campos del siguiente formulario:
 let form = document.querySelector('.register-form');
 
+// capturo los campos del formulario
+let name = form.name;
+let surname = form.surname;
+
+// agrupo según validación
+let notEmpty = [
+    name,
+    surname
+];
+
+let someLenght = [
+    name,
+    surname
+];
+
+let onlyAlpha = [
+    name,
+    surname
+];
+
+// capturo los <div> que van a contener mi <span> con el
+// mensaje de error
+
+
 // creo un objeto que se llama errors:
+let key = '';
 let errors = {};
+errors = [key];
 
-// capturo los campos del formulario y hago las validaciones
+// creo las validaciones que después voy a aplicar a los input
+function checkEmpty(field) {
+    key = field.id;
+    if (!field.value) {
+        field.classList.add('error-input');
+        errors = {
+            [key]: {
+                msg: 'Completa este campo'
+            }
+        };
+    } else {
+        field.classList.remove('error-input');
+        delete errors.field;
+    };
+};
 
-// si alguna de las validaciones que quiero hacer no se cumple,
-//  agrego una propiedad a mi objeto con un mensaje
-errors = {
-    name: {
-        msg: 'completame'
-    }
-}
+function checkLength(field) {
+    key = field.id;
+    if (field.value.length < 2 || field.value.length > 20) {
+        field.classList.add('error-input');
+        errors = {
+            [key]: {
+                msg: 'Ingresa entre 2 y 20 caracteres'
+            }
+        };
+    } else {
+        field.classList.remove('error-input');
+        delete errors.field;
+    };
+};
 
-// también quiero capturar todos los <span> que ya existen en
-// mi vista, y si tengo un span que matchea el id del campo con
-// una de las propiedades de mi objeto errors, le agrego el mensaje 
-// como inner html
+function checkAlpha(field) {
+    key = field.id;
+    if (!(/^[a-zA-Z]*$/).test(field.value)) {
+        field.classList.add('error-input');
+        errors = {
+            [key]: {
+                msg: 'Ingresa solo letras'
+            }
+        };
+    } else {
+        field.classList.remove('error-input')
+        delete errors.field;
+    };
+};
+
+// hago las validaciones sobre los input
+for (let field of notEmpty) {
+    field.addEventListener('blur', () => {
+        checkEmpty(field);
+    });
+};
+
+for (let field of someLenght) {
+    field.addEventListener('input', () => {
+        checkLength(field);
+    });
+};
+
+for (let field of onlyAlpha) {
+    field.addEventListener('input', () => {
+        checkAlpha(field);
+    });
+};
 
 // lo último que quiero hacer es preguntar si mi objeto tiene
 // alguna propiedad (error). si tiene, aquí es donde agrego
 // el mensaje a los span de error. si errores está vacío,
 // envío el formulario
 form.addEventListener('submit', (e) => {
+    for (let field of notEmpty) {
+        checkEmpty(field);
+    };
     if (Object.keys(errors).length > 0) {
         console.log(errors);
         e.preventDefault();
     } else {
         form.submit();
-    }
-})
+    };
+});
