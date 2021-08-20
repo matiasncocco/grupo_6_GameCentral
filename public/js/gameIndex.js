287bce23904efa864684ffd6e9e8a7d05eef563a
santiagoGuastavino <san.guasta@gmail.com>
2021-08-20 17:25:46 -0300

    made some style changes

--- public/js/gameIndex.js
+++ public/js/gameIndex.js
@@ -1,18 +1,29 @@
 let searchBox = document.querySelector('.search-bar-box');
 let searchBar = document.querySelector('.search-bar');
-let searchButton = document.querySelector('.search-button');
 
 searchBox.addEventListener('mouseenter', () => {
     searchBar.classList.add('search-button-hover-me');
-    searchButton.classList.add('search-button-hover-me');
 });
 
 searchBox.addEventListener('mouseout', () => {
     searchBar.classList.remove('search-button-hover-me');
-    searchButton.classList.remove('search-button-hover-me');
 });
 
 searchBox.addEventListener('mouseover', () => {
     searchBar.classList.add('search-button-hover-me');
-    searchButton.classList.add('search-button-hover-me');
+});
+
+let games = document.querySelectorAll('.unit-product');
+
+let userInput = '';
+
+searchBar.addEventListener('input', (e) => {
+    let userInput = searchBar.value.toUpperCase();
+    for (let game of games) {
+        if (game.id.includes(userInput)) {
+            game.classList.remove('hide-while-searching');
+        } else {
+            game.classList.add('hide-while-searching');
+        };
+    };
 });
