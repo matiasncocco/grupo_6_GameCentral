7af14135f5ae1724e5d97ee6e91dbc263b806f99
santiagoGuastavino <san.guasta@gmail.com>
2021-08-21 22:36:33 -0300

    started react app

---
+++ src/dashboard/src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import './assets/css/styles.css'
+
+let Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1>
+                Sidebar
+            </h1>
+            <h2>
+                Sidebar
+            </h2>
+        </div>
+    )
+};
+
+export default Sidebar;
