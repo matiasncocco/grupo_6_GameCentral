f7f600b396b61f8cd6fa880f1a3b74f4e519a145
santiagoGuastavino <san.guasta@gmail.com>
2021-08-31 19:29:13 -0300

    search bar js vanilla

--- public/js/gameIndex.js
+++ public/js/gameIndex.js
@@ -1,33 +1,36 @@
-let searchBox = document.querySelector('.search-bar-box');
+// let searchBox = document.querySelector('.search-bar-box');
 let searchBar = document.querySelector('.search-bar');
 // let searchButton = document.querySelector('.search-button');
 
-searchBox.addEventListener('mouseenter', () => {
-    searchBar.classList.add('search-button-hover-me');
-    // searchButton.classList.add('search-button-hover-me');
-});
+// searchBox.addEventListener('mouseenter', () => {
+//     searchBar.classList.add('search-button-hover-me');
+//     // searchButton.classList.add('search-button-hover-me');
+// });
 
-searchBox.addEventListener('mouseout', () => {
-    searchBar.classList.remove('search-button-hover-me');
-    // searchButton.classList.remove('search-button-hover-me');
-});
+// searchBox.addEventListener('mouseout', () => {
+//     searchBar.classList.remove('search-button-hover-me');
+//     // searchButton.classList.remove('search-button-hover-me');
+// });
 
-searchBox.addEventListener('mouseover', () => {
-    searchBar.classList.add('search-button-hover-me');
+// searchBox.addEventListener('mouseover', () => {
+    // searchBar.classList.add('search-button-hover-me');
     // searchButton.classList.add('search-button-hover-me');
-});
+// });
 
 let games = document.querySelectorAll('.unit-product');
 
 let userInput = '';
 
-searchBar.addEventListener('input', (e) => {
+searchBar.addEventListener('input', () => {
     let userInput = searchBar.value.toUpperCase();
     for (let game of games) {
+        game.style.display = 'none'
+    }
+    for (let game of games) {
         if (game.id.includes(userInput)) {
-            game.classList.remove('hide-while-searching');
+            game.style.display = 'block';
         } else {
-            game.classList.add('hide-while-searching');
+            game.style.display = 'none'
         };
     };
 });
