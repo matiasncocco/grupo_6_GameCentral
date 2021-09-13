1b03540f0d1b8f2e614fd53056859aacc075a693
santiagoGuastavino <san.guasta@gmail.com>
2021-09-13 17:13:22 -0300

    heroku deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
+    "username": "root",
+    "password": null,
+    "database": "game_central",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
     "username": "bc6e2b72c7624e",
     "password": "46f081d2",
     "database": "heroku_e6cd78a42499757",
     "host": "us-cdbr-east-04.cleardb.com",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
     "dialect": "mysql"
   }
 };
