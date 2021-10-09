f9f9368f1413f7c596d8fb7aeb9d5daf8ea6a1c7
santiagoGuastavino <san.guasta@gmail.com>
2021-10-09 17:03:37 -0300

    typo fix

--- src/dashboard/src/utils/helper.js
+++ src/dashboard/src/utils/helper.js
@@ -3,6 +3,6 @@ export let getCurrentUrl = () => {
     if (current.includes('localhost')) {
         return current = 'http://localhost:3001';
     } else {
-        return current = 'https://g6-game-central.herokuapp.com/';
+        return current = 'https://g6-game-central.herokuapp.com';
     };
 };
